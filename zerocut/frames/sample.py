a636ebf871f1ee1949c3a53d32ba190b031ca8b4
ZeroOneDeveloper <me@zero0ne.dev>
2025-06-23 19:32:01 +0900

    chore: method of importlib to importing absolute path

--- zerocut/frames/sample.py
+++ zerocut/frames/sample.py
@@ -1,16 +1,14 @@
 from PIL import Image
 
-from ..frame import Frame
+import os
 
-from importlib.resources import files
+from ..frame import Frame
 
 
 class SimpleWhiteFrame(Frame):
     def __init__(self):
         super().__init__(
-            frame_image=Image.open(
-                files("zerocut/frames/samples/white.png").joinpath("white.png")
-            ).convert("RGB"),
+            frame_image=Image.open(os.path.join(__file__, "white.png")).convert("RGB"),
             positions=[(64, 64), (64, 628), (64, 1192), (64, 1756)],
             photo_size=(876, 500),
         )
@@ -19,9 +17,7 @@ class SimpleWhiteFrame(Frame):
 class SimpleBlackFrame(Frame):
     def __init__(self):
         super().__init__(
-            frame_image=Image.open(
-                files("zerocut/frames/samples/black.png").joinpath("black.png")
-            ).convert("RGB"),
+            frame_image=Image.open(os.path.join(__file__, "black.png")).convert("RGB"),
             positions=[(64, 64), (64, 628), (64, 1192), (64, 1756)],
             photo_size=(876, 500),
         )
