52c610371753b25962c0f0790b14d12b4b6bbfa0
ZeroOneDeveloper <me@zero0ne.dev>
2025-06-23 19:35:22 +0900

    fix: typo

--- zerocut/frames/sample.py
+++ zerocut/frames/sample.py
@@ -8,7 +8,7 @@ from ..frame import Frame
 class SimpleWhiteFrame(Frame):
     def __init__(self):
         super().__init__(
-            frame_image=Image.open(os.path.join(os.path.dirname(__file__), "white.png")).convert("RGB"),
+            frame_image=Image.open(os.path.join(os.path.dirname(__file__), "samples", "white.png")).convert("RGB"),
             positions=[(64, 64), (64, 628), (64, 1192), (64, 1756)],
             photo_size=(876, 500),
         )
@@ -17,7 +17,7 @@ class SimpleWhiteFrame(Frame):
 class SimpleBlackFrame(Frame):
     def __init__(self):
         super().__init__(
-            frame_image=Image.open(os.path.join(os.path.dirname(__file__), "black.png")).convert("RGB"),
+            frame_image=Image.open(os.path.join(os.path.dirname(__file__), "samples", "black.png")).convert("RGB"),
             positions=[(64, 64), (64, 628), (64, 1192), (64, 1756)],
             photo_size=(876, 500),
         )
